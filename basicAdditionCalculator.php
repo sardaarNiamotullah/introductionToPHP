63d43f84a9a2c04077ea9a3010312ad9b2890bf4
sardaarNiamotullah <sardaar.niamotullah@gmail.com>
2022-01-31 03:21:44 +0600

    fix bug and add some design

--- basicAdditionCalculator.php
+++ basicAdditionCalculator.php
@@ -7,60 +7,90 @@
     <title>Basic Addition Calculator</title>
 </head>
 <style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
     body {
         font-family: Verdana, Geneva, Tahoma, sans-serif;
-        background-color: white;
-        padding: 30px;
         font-weight: bold;
+        padding: 30px 200px;
     }
-    header {
-        text-align: center;
-        height: 20vh;
-    }
+
     .main {
+        height: 90vh;
         text-align: center;
+        background-color: #C7B198;
+        color: #191A19;
+        border-radius: 10px;
     }
+    .welcome {
+        margin-bottom: 80px;
+        padding-top: 80px;
+    }
+
+    .code {
+        margin-top: 40px;
+    }
+
     .intro {
         display: block;
         font-size: 20px;
     }
     .blank {
-        height: 5px;
+        height: 15px;
     }
     hr {
-        width: 30%;
+        width: 25%;
+        align-items: center;
+        margin-block-start: 0.5em;
+        margin-block-end: 0.5em;
+        margin-inline-start: auto;
+        margin-inline-end: auto;
     }
     input {
         margin-top: 15px;
         margin-bottom: 15px;
         width: 70px;
     }
+    input:last-child {
+        width: 150px;
+    }
+    .result {
+        color: red;
+        margin-top: 20px;
+        font-size: 50px;
+    }
 
 </style>
 <body>
-    <header>
-        <h1>
-            <span class="intro">Assalamualikum</span>
-            <span class="intro">We can help you to add two numbers</span>
-        </h1>
-    </header>
     <section class="main">
+
+        <h1 class="welcome">
+            <span class="intro">Assalamualikum</span>
+            <span class="intro">We can help you to add two numbers for free!!!</span>
+        </h1>
+
         <form action="basicAdditionCalculator.php" method="$_GET">
             <span>Please insert your 1st number: </span> <br>
-            <input type="number" name="firstNumber"> <br>
+            <input type="number" name="firstNumber" step="any"> <br>
             <span>Please insert your 2nd number: </span> <br>
-            <input type="number" name="secondNumber"> <br>
+            <input type="number" name="secondNumber" step="any"> <br>
             <p class="blank"></p>
             <hr>
-            <input type="submit">
+            <input type="submit" value="Perform Addition">
         </form>
-    </section>
-    <section class="result">
-        <p>The addition of your two numbers is: 
-            <?php 
-                echo ($_GET["firstNumber"] + $_GET["secondNumber"]);
-            ?>
-        </p>
+
+        <div class="code">
+        <?php
+            if(empty($_GET["firstNumber"]) || empty($_GET["secondNumber"])) {
+            } else {
+                $result = $_GET["firstNumber"] + $_GET["secondNumber"];
+                echo "<p>The result of the addition of your two number is:</p> <h1 class='result'>$result</h1>";
+            }
+        ?>
+        </div>
+
     </section>
 </body>
 </html>
